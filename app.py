642f87716b6852d89c99ff58e0373b490c4aa917
ageuribeiro <ageu87@gmail.com>
2023-07-01 17:42:53 -0300

    Configurando credencial firebase.

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+import requests 
+import json
+import pdfkit 
+import datetime
+import os
+import firebase_admin
+import string
+import firebase_admin
 from flask import Flask, render_template, request, redirect, flash, url_for, session
-from firebase_admin import auth
-import requests, json, pdfkit, datetime, os, firebase_admin, pyrebase
+from firebase_admin import auth, credentials
+from flask_session import Session
+from datetime import timedelta
 
-firebase_admin.initialize_app()
+
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 app.config['UPLOAD_FOLDER'] = 'files'
+app.config['SESSION_TYPE'] ='filesystem'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USER_SIGNER'] = True
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['PERMANET_SESSION_LIFETIME'] = timedelta(minutes=5)
 
+Session(app)
 
 config ={
     'apiKey': "AIzaSyCuATOltNI_Vxu_ucfzXPNmN2V1puvqABU",
@@ -18,59 +34,14 @@ config ={
     'appId': "1:365589111987:web:cc51e2470cbe43ec40fcc6"
 }
 
-firebase = pyrebase.initialize_app(config)
+cred = credentials.Certificate('Git/key/credencial.json')
+firebase = firebase_admin.initialize_app(cred)
 auth = firebase.auth()
 
 app.secret_key = 'secret'
 
 # URL do banco de dados do firebase
-firebase_url = "https://appweb-orion-php-default-rtdb.firebaseio.com/"
-
-member_dados = {
-  "id": 0,
-  "nome": "null",
-  "pai": "null",
-  "mae": "null",
-  "data_nasc": "null",
-  "estado_civil": "null",
-  "cpf": "null",
-  "rg": "null",
-  "setor_atual": "null",
-  "igreja_atual": "null",
-  "setor_anterior": "null",
-  "igreja_anterior": "null",
-  "batizado_com_espirito_santo": "null",
-  "escolaridade": "null",
-  "profissao": "null",
-  "batizado_nas_aguas": "null",
-  "data_batismo_nas_aguas": "null",
-  "igreja_de_batismo": "null",
-  "admitido_por": "null",
-  "data_da_consagracao": "null",
-  "data_da_apresentacao": "null",
-  "cargo_na_igreja": "null",
-  "endereco": "null",
-  "bairro": "null",
-  "cidade": "null",
-  "estado": "null",
-  "cep": "null",
-  "telefone": "null",
-  "email": "null",
-  "tem_cartao_de_membro": "null",
-  "foto": "null"
-}
-
-# criar membro
-req_create_member = requests.post(f'{firebase_url}/membros/.json', data = json.dumps(member_dados))
-
-# index
-@app.route("/")
-def index():
-    
-    # Obter todos os usuarios do firebase
-    response = requests.get(f'{firebase_url}/usuarios.json')
-    users = response.json() if response.ok else {}
-    return render_template("index.html", users=users)
+firebase_url = "https://appweb-orion-php-default-rtdb.firebaseio.com/members.json"
 
 # ======================== Login ==============================================================================
 
@@ -78,7 +49,8 @@ def index():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if('user' in session):
-        return 'Hi, {}'.format(session['user'])
+        return render_template('membros/read.html')
+        gretting = 'Hi, {}'.format(session['user'])
 
     if request.method == "POST":
         email = request.form.get("email")
@@ -90,7 +62,7 @@ def login():
             session['user'] = email
 
             flash("Login realizado com sucesso.","success")
-            return redirect(url_for("list_member"))
+            return redirect(url_for("index"))
     
         except:
             # Trate os erros de autenticação do Firebase
@@ -102,106 +74,46 @@ def login():
 @app.route("/logout")
 def logout():
     session.pop('user')
+    session.clear()
     return redirect(url_for("index"))
 
-# ======================== Usuarios ==============================================================================
+#============================ Funções de configuração ===========================================================================================
 
-# listar usuarios
-@app.route("/list_user")
-def list_user():
-    con = sql.connect("app_orion_database.db")
-    con.row_factory=sql.Row
-    cur=con.cursor()
-    cur.execute("select * from users")
-    data=cur.fetchall()
+# gerar id automaticamente
+def generate_id():
+    characters = string.ascii_uppercase + string.digits
+    id = ''.join(random.choices(characters, k=16))
+    return id
 
-    user_id = session.get('user_id')
-    if user_id is None:
-        flash("Você precisa fazer login para acessar esta página.", "danger")
-        return redirect(url_for("login"))
+# verificar se o id gerado já existe
+def check_id_exists(id):
+    # Fazer a solicitação GET para obter os daods do nó "members"
+    response = requests.get(f'{firebase_url}/members.json')
 
-    return render_template("usuarios/read.html", datas=data, user_id = user_id)
-
-# adicionar usuario
-@app.route("/add_user", methods=["POST", "GET"])
-def add_user():
-    if request.method == "POST":
-        nome = request.form["nome"]
-        email = request.form["email"]
-        usuario =request.form["usuario"]
-        senha = request.form["senha"]
-
-        # Criar o objeto de usuario
-        user_data = {
-            "nome":nome, 
-            "email":email, 
-            "senha":senha
-            }
-        # Enviar a requisição POST para criar o usuario no Firebase
-        response = requests.post(f'{firebase_url}/usuarios.json', json=user_data)
-
-        if response.ok:
-            
-            return redirect(url_for('index'))
-            flash("Dados cadastrados com sucesso.","success")
-        else:
-            return "Erro ao criar novo usuario."
-    return render_template("usuarios/read.html")
-
-# editar usuario        
-@app.route("/edit_user/<string:user_id>", methods=["POST", "GET"])
-def edit_user(user_id):
+    if response.status_code == 200:
     
-    response = requests.get(f'{firebase_url}/usuarios/{user_id}.json')
-    user_data = response.json() if response.ok else {}
+        # Verificar se o id já existe no nó membros
+        members = response.json()
+        if members:
+            for member_id in members:
+                if members[member_id]['key'] == id:
+                    return True
+    return False
 
-    if request.method == "POST":
-        # Obter os dados do formulario
-        nome = request.form["nome"]
-        email = request.form["email"]
-        usuario =request.form["usuario"]
-        senha = request.form["senha"]
+# ======================== Funções CRUD para Membros ==============================================================================
+# Rota para exibir os dados
+@app.route("/")
+def index():
+    # verificar se o usuário está autenticado
+    if 'user' not in session:
+        return redirect(url_for('login'))
 
+    data = read_data()
+    return render_template('index.html', data=data)
 
-        # Atualizar os dados do  usuario
-        user_data["nome"] = nome
-        user_data["email"] = email
-        user_data["usuario"] = usuario
-        user_data["senha"] = senha
-
-        # Enviar a requisição PUT para atualizar o usuario no firebase
-        response = requests.put(f"{firebase_url}/usuarios/{user_id}.json", json=user_data)
-
-        if response.ok:
-            return redirect(url_for("index"))
-        else:
-            return "Erro ao atualizar usuário."
-        
-    return render_template("usuarios/update.html", user_id=user_id, user=user_data)
-
-# deletar usuario
-@app.route("/delete_user/<string:user_id>", methods=["POST"])
-def delete_user(user_id):
-    
-    # Enviar a requisição DELETE para remover o usuaario do Firebase
-    response = requests.delete(f'{firebase_url}/usuarios/{user_id}.json')
-
-    if reponse.ok:
-        return redirect(url_for("index"))
-    else:
-        return "Erro ao excluir usuario."
-    
-#============================ Membros ===========================================================================================
-
-# listar membro
-@app.route("/list_member")
-def list_member():
-
-    return render_template("membros/read.html")
-
-#adicionar membro
-@app.route("/add_member", methods=["POST", "GET"])
-def add_member():
+# Rota para adicionar dados
+@app.route("/add", methods=['GET','POST'])
+def add():
     if request.method == "POST":
         nome = request.form["nome"]
         pai = request.form["pai"]
@@ -233,51 +145,40 @@ def add_member():
         email = request.form["email"]
         tem_cartao = request.form["tem_cartao"]
         foto = request.files["foto"]
-
-        # formatando a data de nascimento
-        data_nascimento_format = datetime.datetime.strptime(data_nasc, "%Y-%m-%d").date()
-        formatted_date_nasc = data_nascimento_format.strftime("%d/%m/%Y")
-
-        # formatando a data de batismo
-        data_batismo_format = datetime.datetime.strptime(data_batismo, "%Y-%m-%d").date()
-        formatted_date_batismo = data_batismo_format.strftime("%d/%m/%Y")
-
-        # formatando a data da consagração
-        data_da_consagracao_format = datetime.datetime.strptime(data_da_consagracao, "%Y-%m-%d").date()
-        formatted_data_da_consagracao = data_da_consagracao_format.strftime("%d/%m/%Y")
+        id = generate_id()
         
-        # formatando a data da apresentação
-        data_da_apresentacao_format = datetime.datetime.strptime(data_da_apresentacao, "%Y-%m-%d").date()
-        formatted_data_da_apresentacao = data_da_apresentacao_format.strftime("%d/%m/%Y")
+        # Verificar se o ID já existe
+        while check_id_exists(id):
+            id = generate_id()
 
+        # Salvar a imagem em um diretorio local
         # Criar diretorio com nome do membro
         nome_less = nome.replace(" ","")
-        dir_membro = os.path.join('static','files', nome_less) 
+        dir_membro = os.path.join('static', 'files', nome_less) 
         os.makedirs(dir_membro, exist_ok=True)
 
         # Salvar Imagem
         imagem_path = os.path.join(dir_membro, nome_less + '.png')
         foto.save(imagem_path)
 
-        # Inserir no banco de dados
-        con = sql.connect("app_orion_database.db")
-        cur = con.cursor()
-        cur.execute("insert into members(NOME, PAI, MAE, DATA_NASC, ESTADO_CIVIL, CPF, RG, SETOR_ATUAL, IGREJA_ATUAL, SETOR_ANTERIOR, IGREJA_ANTERIOR, BATIZADO_COM_ESPIRITO_SANTO, ESCOLARIDADE, PROFISSAO, BATIZADO_NAS_AGUAS, DATA_BATISMO_NAS_AGUAS, IGREJA_DE_BATISMO, ADMITIDO_POR, DATA_DA_CONSAGRACAO, DATA_DA_APRESENTACAO, CARGO_NA_IGREJA, ENDERECO, BAIRRO, CIDADE, ESTADO, CEP, TELEFONE, EMAIL, TEM_CARTAO_DE_MEMBRO, FOTO) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(nome, pai, mae,formatted_date_nasc, estado_civil, cpf, rg, setor_atual, igreja_atual, setor_anterior, igreja_anterior, batizado_com_espirito_santo, escolaridade, profissao, batizado, formatted_date_batismo, igreja_de_batismo, admitido_por, formatted_data_da_consagracao, formatted_data_da_apresentacao, cargo_na_igreja, endereco, bairro, cidade, estado, cep, telefone, email, tem_cartao, imagem_path))
-        con.commit()
+        # Salvar os dados no firebase
+        data = {'id':id, 'nome':nome, 'pai':pai,'mae':mae, 'data_nasc':data_nasc, 'estado_civil':estado_civil, 'cpf':cpf, 'rg':rg,'setor_atual':setor_atual, 'igreja_atual':igreja_atual, 'setor_anterior':setor_anterior, 'igreja_anterior':igreja_anterior, 'batizado_com_espirito_santo':batizado_com_espirito_santo, 'escolaridade':escolaridade, 'profissao':profissao,'batizado':batizado,'data_batismo':data_batismo, 'igreja_de_batismo':igreja_de_batismo, 'admitido_por':admitido_por, 'data_da_consagracao':data_da_consagracao, 'data_da_apresentacao':data_da_apresentacao, 'cargo_na_igreja':cargo_na_igreja, 'endereco':endereco, 'bairro':bairro, 'cidade':cidade, 'estado':estado, 'cep':cep, 'telefone':telefone, 'email':email, 'tem_cartao':tem_cartao, 'foto': imagem_path }
+        create_data(data)
 
         # Exibir mensagem de flash
         flash("Dados cadastrados com sucesso.","success")
 
-        # Redirecionar para o PDF gerado
-        return redirect(url_for('list_member'))
+        # Redirecionar para a lista de membros para gerar pdf
+        return redirect(url_for('index'))
+    return render_template('membros/add.html')
 
-    
     user_id = session.get('user_id')
     if user_id is None:
-        flash("Você precisa fazer login para acessar esta página.", "danger")
+        flash("Você precisa fazer login para acessar esta página. \n\r Estamos redirecionando para a página de login.", "danger")
+        
         return redirect(url_for("login"))
 
-    return render_template("membros/add_member.html", user_id = user_id)
+    return render_template("add.html", user_id = user_id)
 
 
 # Rota para exibir os dados do membro e gerar o PDF
@@ -291,7 +192,7 @@ def view_member(id):
 
     if member_data is None:
         flash("Membro não encontrado.", "danger")
-        return redirect(url_for("list_member"))
+        return redirect(url_for("index"))
     
     return render_template("membros/view_member.html", member_data = member_data)
 
@@ -397,6 +298,45 @@ def list_report():
         return redirect(url_for("login"))
 
     return render_template("membros/list_report.html", datas=data, user_id = user_id)
+
+#============================ Funções para os métodos HTTP ===========================================================================================
+
+# Função para criar os dados
+def create_data(data):
+    response = requests.post(firebase_url, json.dumps(data))
+    if response.status_code != 200:
+        print('Erro ao criar dados.')
+
+# Função para ler os dados
+def read_data():
+    response = requests.get(firebase_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print('Erro ao ler dados.')
+
+# Função para atualizar dados
+def update_data(key, new_data):
+    url_update = f"https://appweb-orion-php-default-rtdb.firebaseio.com/members/{key}.json"
+    response = requests.patch(url_update, json.dumps(new_data))
+    if response.status_code != 200:
+        print('Erro ao atualizar dados.')
+
+# Função para deletar dados
+def delete_data(key):
+    url_delete = f"https://appweb-orion-php-default-rtdb.firebaseio.com/members/{key}.json"
+    response = requests.delete(url_delete)
+    if response.status_code == 200:
+        print('Dados excluídos com sucesso.')
+    else:
+        print('Erro ao excluir dados.')
+
+
+# Rota para exibir os dados do membro e gerar o PDF
+@app.route("/report/<key>")
+def report(key):
+   return render_template("index.html")
 
 if __name__=='__main__':
     app.run(debug=True, port=911)
