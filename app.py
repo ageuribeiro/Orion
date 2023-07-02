26f6ce6241a389547483cdcec4e9d7405d52d9d0
ageuribeiro <ageu87@gmail.com>
2023-07-01 22:56:28 -0300

    Solve erro in request

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import json
 import pdfkit 
 import datetime
 import os
-import firebase_admin
+import pyrebase
 import string
-import firebase_admin
+import time
+
 from flask import Flask, render_template, request, redirect, flash, url_for, session
-from firebase_admin import auth, credentials
+
 from flask_session import Session
 from datetime import timedelta
 
@@ -34,48 +35,43 @@ config ={
     'appId': "1:365589111987:web:cc51e2470cbe43ec40fcc6"
 }
 
-cred = credentials.Certificate('Git/key/credencial.json')
-firebase = firebase_admin.initialize_app(cred)
+
+firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 app.secret_key = 'secret'
 
 # URL do banco de dados do firebase
-firebase_url = "https://appweb-orion-php-default-rtdb.firebaseio.com/members.json"
+url = "https://appweb-orion-php-default-rtdb.firebaseio.com/members.json"
 
-# ======================== Login ==============================================================================
+# ======================== session user ==============================================================================
 
 # login session 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def login():
     if('user' in session):
-        return render_template('membros/read.html')
-        gretting = 'Hi, {}'.format(session['user'])
+        return render_template("read.html")
 
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        
+
         try:
-            # Autentique o usuário com o Firebase Authentication
+
             user = auth.sign_in_with_email_and_password(email, password)
             session['user'] = email
-
-            flash("Login realizado com sucesso.","success")
-            return redirect(url_for("index"))
-    
         except:
-            # Trate os erros de autenticação do Firebase
-            flash("Failed to login. Try again later.", "danger")
-
+            flash('Failed to login','danger')
+            time.sleep(5)
+            return flash('Failed to login','danger')
     return render_template("index.html")
 
 # logout session    
 @app.route("/logout")
 def logout():
-    session.pop('user')
+    session.pop('user', None)
     session.clear()
-    return redirect(url_for("index"))
+    return redirect(url_for('login'))
 
 #============================ Funções de configuração ===========================================================================================
 
@@ -88,7 +84,7 @@ def generate_id():
 # verificar se o id gerado já existe
 def check_id_exists(id):
     # Fazer a solicitação GET para obter os daods do nó "members"
-    response = requests.get(f'{firebase_url}/members.json')
+    response = requests.get(f'{url}/members.json')
 
     if response.status_code == 200:
     
@@ -102,49 +98,49 @@ def check_id_exists(id):
 
 # ======================== Funções CRUD para Membros ==============================================================================
 # Rota para exibir os dados
-@app.route("/")
-def index():
+@app.route("/read")
+def read():
     # verificar se o usuário está autenticado
     if 'user' not in session:
         return redirect(url_for('login'))
 
     data = read_data()
-    return render_template('index.html', data=data)
+    return render_template('read.html', data=data)
 
 # Rota para adicionar dados
-@app.route("/add", methods=['GET','POST'])
+@app.route('/add', methods=['GET','POST'])
 def add():
-    if request.method == "POST":
-        nome = request.form["nome"]
-        pai = request.form["pai"]
-        mae =  request.form["mae"]
-        data_nasc = request.form["data_nasc"]
-        estado_civil = request.form["estado_civil"]
-        cpf = request.form["cpf"]
-        rg = request.form["rg"]
-        setor_atual = request.form["setor_atual"]
-        igreja_atual = request.form["igreja_atual"]
-        setor_anterior = request.form["setor_anterior"]
-        igreja_anterior = request.form["igreja_anterior"]
-        batizado_com_espirito_santo = request.form["batizado_com_espirito_santo"]
-        escolaridade = request.form["escolaridade"]
-        profissao = request.form["profissao"]
-        batizado = request.form["batizado"]
-        data_batismo = request.form["data_batismo"]
-        igreja_de_batismo = request.form["igreja_de_batismo"]
-        admitido_por = request.form["admitido_por"]
-        data_da_consagracao = request.form["data_da_consagracao"]
-        data_da_apresentacao = request.form["data_da_apresentacao"]
-        cargo_na_igreja = request.form["cargo_na_igreja"]
-        endereco = request.form["endereco"]
-        bairro = request.form["bairro"]
-        cidade = request.form["cidade"]
-        estado = request.form["estado"]
-        cep = request.form["cep"]
-        telefone = request.form["telefone"]
-        email = request.form["email"]
-        tem_cartao = request.form["tem_cartao"]
-        foto = request.files["foto"]
+    if request.method == 'POST':
+        nome = request.form['nome']
+        pai = request.form['pai']
+        mae =  request.form['mae']
+        data_nasc = request.form['data_nasc']
+        estado_civil = request.form['estado_civil']
+        cpf = request.form['cpf']
+        rg = request.form['rg']
+        setor_atual = request.form['setor_atual']
+        igreja_atual = request.form['igreja_atual']
+        setor_anterior = request.form['setor_anterior']
+        igreja_anterior = request.form['igreja_anterior']
+        batizado_com_espirito_santo = request.form['batizado_com_espirito_santo']
+        escolaridade = request.form['escolaridade']
+        profissao = request.form['profissao']
+        batizado = request.form['batizado']
+        data_batismo = request.form['data_batismo']
+        igreja_de_batismo = request.form['igreja_de_batismo']
+        admitido_por = request.form['admitido_por']
+        data_da_consagracao = request.form['data_da_consagracao']
+        data_da_apresentacao = request.form['data_da_apresentacao']
+        cargo_na_igreja = request.form['cargo_na_igreja']
+        endereco = request.form['endereco']
+        bairro = request.form['bairro']
+        cidade = request.form['cidade']
+        estado = request.form['estado']
+        cep = request.form['cep']
+        telefone = request.form['telefone']
+        email = request.form['email']
+        tem_cartao = request.form['tem_cartao']
+        foto = request.files['foto']
         id = generate_id()
         
         # Verificar se o ID já existe
@@ -170,15 +166,7 @@ def add():
 
         # Redirecionar para a lista de membros para gerar pdf
         return redirect(url_for('index'))
-    return render_template('membros/add.html')
-
-    user_id = session.get('user_id')
-    if user_id is None:
-        flash("Você precisa fazer login para acessar esta página. \n\r Estamos redirecionando para a página de login.", "danger")
-        
-        return redirect(url_for("login"))
-
-    return render_template("add.html", user_id = user_id)
+    return render_template('add.html')
 
 
 # Rota para exibir os dados do membro e gerar o PDF
@@ -197,91 +185,89 @@ def view_member(id):
     return render_template("membros/view_member.html", member_data = member_data)
 
 
-# Rota para gerar PDF do membro
-@app.route("/member/<string:id>/generate_pdf")
-def generate_pdf(id):
-    con = sql.connect("app_orion_database.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM members WHERE ID = ?", (id,))
-    member_data = cur.fetchone()
-    con.close()
-
-    if member_data is None:
-        flash("Membro não encontrado.", "danger")
-        return redirect(url_for("list_member"))
-    else:
-        # Gerar o conteudo HTML no PDF
-        html = render_template("membros/view_member.html", member_data = member_data)
-
-    # Retornar o PDF utilizando o conteudo HTML
-    return send_file(pdf_path, as_attachment=False)
-
-# editar membro        
-@app.route("/edit_member/<string:id>", methods=["POST", "GET"])
-def edit_member(id):
+# Rota para editar dados      
+@app.route('/edit/<key>', methods=['GET', 'POST'])
+def edit_member(key):
     if request.method=="POST":
-        nome = request.form["nome"]
-        pai = request.form["pai"]
-        mae =  request.form["mae"]
-        data_nasc = request.form["data_nasc"]
-        estado_civil = request.form["estado_civil"]
-        cpf = request.form["cpf"]
-        rg = request.form["rg"]
-        setor_atual = request.form["setor_atual"]
-        igreja_atual = request.form["igreja_atual"]
-        setor_anterior = request.form["setor_anterior"]
-        igreja_anterior = request.form["igreja_anterior"]
-        batizado_com_espirito_santo = request.form["batizado_com_espirito_santo"]
-        escolaridade = request.form["escolaridade"]
-        profissao = request.form["profissao"]
-        batizado = request.form["batizado"]
-        data_batismo = request.form["data_batismo"]
-        igreja_de_batismo = request.form["igreja_de_batismo"]
-        admitido_por = request.form["admitido_por"]
-        data_da_consagracao = request.form["data_da_consagracao"]
-        data_da_apresentacao = request.form["data_da_apresentacao"]
-        cargo_na_igreja = request.form["cargo_na_igreja"]
-        endereco = request.form["endereco"]
-        bairro = request.form["bairro"]
-        cidade = request.form["cidade"]
-        estado = request.form["estado"]
-        cep = request.form["cep"]
-        telefone = request.form["telefone"]
-        email = request.form["email"]
-        tem_cartao = request.form["tem_cartao"]
-        con = sql.connect("app_orion_database.db")
-        cur=con.cursor()
-        cur.execute("update members set NOME = ?, PAI = ?, MAE = ?, DATA_NASC = ?, ESTADO_CIVIL = ?, CPF = ?, RG = ?, SETOR_ATUAL = ?, IGREJA_ATUAL = ?, SETOR_ANTERIOR = ?, IGREJA_ANTERIOR = ?, BATIZADO_COM_ESPIRITO_SANTO = ?, ESCOLARIDADE = ?, PROFISSAO = ?, BATIZADO_NAS_AGUAS = ?, DATA_BATISMO_NAS_AGUAS = ?, IGREJA_DE_BATISMO = ? , ADMITIDO_POR = ?, DATA_DA_CONSAGRACAO = ?, DATA_DA_APRESENTACAO = ?, CARGO_NA_IGREJA = ?, ENDERECO = ?, BAIRRO = ?, CIDADE = ?, ESTADO = ?, CEP = ?, TELEFONE = ?, EMAIL = ?, TEM_CARTAO_DE_MEMBRO = ? WHERE ID = ? ",(nome, pai, mae, data_nasc, estado_civil, cpf, rg, setor_atual, igreja_atual, setor_anterior, igreja_anterior, batizado_com_espirito_santo, escolaridade, profissao, batizado, data_batismo, igreja_de_batismo, admitido_por, data_da_consagracao, data_da_apresentacao, cargo_na_igreja, endereco, bairro, cidade, estado, cep, telefone, email, tem_cartao, id))
-        con.commit()
+        new_nome = request.form["nome"]
+        new_pai = request.form["pai"]
+        new_mae =  request.form["mae"]
+        new_data_nasc = request.form["data_nasc"]
+        new_estado_civil = request.form["estado_civil"]
+        new_cpf = request.form["cpf"]
+        new_rg = request.form["rg"]
+        new_setor_atual = request.form["setor_atual"]
+        new_igreja_atual = request.form["igreja_atual"]
+        new_setor_anterior = request.form["setor_anterior"]
+        new_igreja_anterior = request.form["igreja_anterior"]
+        new_batizado_com_espirito_santo = request.form["batizado_com_espirito_santo"]
+        new_escolaridade = request.form["escolaridade"]
+        new_profissao = request.form["profissao"]
+        new_batizado = request.form["batizado"]
+        new_data_batismo = request.form["data_batismo"]
+        new_igreja_de_batismo = request.form["igreja_de_batismo"]
+        new_admitido_por = request.form["admitido_por"]
+        new_data_da_consagracao = request.form["data_da_consagracao"]
+        new_data_da_apresentacao = request.form["data_da_apresentacao"]
+        new_cargo_na_igreja = request.form["cargo_na_igreja"]
+        new_endereco = request.form["endereco"]
+        new_bairro = request.form["bairro"]
+        new_cidade = request.form["cidade"]
+        new_estado = request.form["estado"]
+        new_cep = request.form["cep"]
+        new_telefone = request.form["telefone"]
+        new_email = request.form["email"]
+        new_tem_cartao = request.form["tem_cartao"]
+
+        new_data = {
+            "nome": new_nome,
+            "pai": new_pai,
+            "mae": new_mae,
+            "data_nasc": new_data_nasc,
+            "estado_civil": new_estado_civil,
+            "cpf": new_cpf,
+            "rg": new_rg,
+            "setor_atual": new_setor_atual,
+            "igreja_atual": new_igreja_atual,
+            "setor_anterior": new_setor_anterior,
+            "igreja_anterior": new_igreja_anterior,
+            "batizado_com_espirito_santo": new_batizado_com_espirito_santo,
+            "escolaridade": new_escolaridade,
+            "profissao": new_profissao,
+            "batizado": new_batizado,
+            "data_batismo": new_data_batismo,
+            "igreja_de_batismo": new_igreja_de_batismo,
+            "admitido_por": new_admitido_por,
+            "data_da_consagracao": new_data_da_consagracao,
+            "data_da_apresentacao": new_data_da_apresentacao,
+            "cargo_na_igreja": new_cargo_na_igreja,
+            "endereco": new_endereco,
+            "bairro": new_bairro,
+            "cidade": new_cidade,
+            "estado": new_estado,
+            "cep": new_cep,
+            "telefone": new_telefone,
+            "email": new_email,
+            "tem_cartao": new_tem_cartao
+        }    
+
+        update_data(key, new_data)
+        
         flash("Dados atualizados com sucesso","success")
-        return redirect(url_for("list_member"))
-    con = sql.connect("app_orion_database.db")
-    con.row_factory = sql.Row
-    cur = con.cursor()
-    cur.execute("select * from members where ID = ?",(id,))
-    data = cur.fetchone()
-    return render_template("membros/edit_member.html", datas=data)
+        return redirect(url_for("read"))
+    else:
+        data = read_data()
+        if key in data:
+            return render_template("edit.html", key=key, data=data[key])
+        else:
+            return "Invalid Key."
 
-# deletar member
-@app.route("/delete_member/<string:id>", methods=["GET"])
-def delete_member(id):
-    con = sql.connect("app_orion_database.db")
-    cur = con.cursor()
-    cur.execute("delete from members WHERE ID=?", (id,))
-    con.commit()
-    flash("Dados deletados.","warning")
-    return redirect(url_for("list_member"))
-
-# criar relatorio do membro
-@app.route("/report_member/<string:id>", methods=["GET"])
-def report_member(id):
-    con = sql.connect("app_orion_database.db")
-    cur = con.cursor()
-    cur.execute("select * from members WHERE ID=?",(id))
-    con.commit()
-    flash("Relatório criado com sucesso","success")
-    return redirect(url_for('list_member'))
-
+# Rota para deletar dados
+@app.route('/delete/<key>')
+def delete(key):
+    delete_data(key)
+    flash('Dados deletados com sucesso.','success')
+    return redirect(url_for('read'))
 
 # listar relatorio do membro
 @app.route("/list_report")
@@ -299,22 +285,33 @@ def list_report():
 
     return render_template("membros/list_report.html", datas=data, user_id = user_id)
 
+
+# Rota para exibir os dados do membro e gerar o PDF
+@app.route("/report/<key>")
+def report(key):
+   return render_template("index.html")
 #============================ Funções para os métodos HTTP ===========================================================================================
 
 # Função para criar os dados
 def create_data(data):
-    response = requests.post(firebase_url, json.dumps(data))
+    response = requests.post(url, json.dumps(data))
     if response.status_code != 200:
         print('Erro ao criar dados.')
 
 # Função para ler os dados
 def read_data():
-    response = requests.get(firebase_url)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
         data = response.json()
         return data
-    else:
-        print('Erro ao ler dados.')
+    except requests.exceptions.RequestException as e:
+        print('Erro ao fazer a requisição:', str(e))
+        return None
+    except ValueError as e:
+        print('Erro ao processar a resposta JSON: ', str(e))
+        return None
 
 # Função para atualizar dados
 def update_data(key, new_data):
@@ -333,10 +330,7 @@ def delete_data(key):
         print('Erro ao excluir dados.')
 
 
-# Rota para exibir os dados do membro e gerar o PDF
-@app.route("/report/<key>")
-def report(key):
-   return render_template("index.html")
+
 
 if __name__=='__main__':
-    app.run(debug=True, port=911)
+    app.run(debug=True)
--- authentication.py
+++ authentication.py
@@ -20,10 +20,11 @@ password = "ageu123456789"
 # user = auth.create_user_with_email_and_password(email, password)
 # print(user)
 
+user = auth.sign_in_with_email_and_password(email, password)
+# print(user)
 
-
-info = auth.get_account_info(user['idToken'])
-print(info)
+# info = auth.get_account_info(user['idToken'])
+# print(info)
 
 # auth.send_email_verification(user['idToken'])
 
